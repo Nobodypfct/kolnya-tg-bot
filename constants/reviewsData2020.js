b24e87c54cbbc2dc6f1bb48b1b61b9068c005099
Nikolai Doinikov <56079901+Nobodypfct@users.noreply.github.com>
2022-02-01 10:19:48 +0300

    updated reviews content

--- constants/reviewsData2020.js
+++ constants/reviewsData2020.js
@@ -1,16 +1,16 @@
 const reviews2020 = [
     {
         file_id: 'BAACAgIAAxkBAAMNYfgdAAH8ViA3M5u5Bgxju7ta1oCQAAKGFwACfjfBS9JPxV4RkYZwIwQ',
-        caption: '🔥Отзыв от нашей выпускницы Марианны г. Волгоград👋🏼 \n' +
-        '👇🏼Инст Марианны: lady_style_tattoo'
+        caption: '🔥Отзыв от нашей выпускницы Марианны г. Волгоград👋🏼 \n' + '\n' +
+        '👉🏼Инст Марианны: lady_style_tattoo'
     },
     {
         file_id: 'BAACAgIAAxkBAAMPYfgdq4ieKiKWvWa2GRq1wtlM-lMAApEXAAJ-N8FL3ZXPQuXVWdQjBA',
-        caption: '🔥Отзыв от нашего выпускника🧑🏼‍🎓\n Инста Димы👉🏼artdima'
+        caption: '🔥Отзыв от нашего выпускника🧑🏼‍🎓\n' + '\n' + '👉🏼Инста Димы: artdima'
     },
     {
         file_id: 'BAACAgIAAxkBAAMyYfgg00g6Un2kSJ_amNyYnHIF7M8AAq0XAAJ-N8FLe6vH1QFkgCIjBA',
-        caption: '🔥Отзыв от нашей выпускницы Дарьи г. Москва🚀 👉🏼Инст Даши: cat_patric'
+        caption: '🔥Отзыв от нашей выпускницы Дарьи г. Москва🚀 \n' + '\n' + '👉🏼Инст Даши: cat_patric'
     },
     {
         file_id: 'BAACAgIAAxkBAAM0YfgheNCCA9EMY-B8IC4Wl_zERA8AArQXAAJ-N8FLwRoq53PiYyAjBA',
@@ -22,13 +22,13 @@ const reviews2020 = [
         file_id: 'BAACAgIAAxkBAAM2Yfgh0sGXs_GWR20PmuWam6zTb1MAAroXAAJ-N8FLDiCvFKipA38jBA',
         caption: '🔥Отзыв от нашего ученика Ивана👩‍🎓. Парню 17 лет👶🏼, приехал к нам из Краснодара☀️. Ряд упорных тренировок под руководством Саши Кота и результат на лицо💪🏼😎 \n' +
         '\n' +
-        'Инста Вани: wvvdv'
+        '👉🏼Инста Вани: wvvdv'
     },
     {
         file_id: 'BAACAgIAAxkBAAM4YfgiP2rwbONVy6AFX2SQ7fndGmIAAr8XAAJ-N8FLBmblleAm5nkjBA',
         caption: '👋🏼Отзыв от ученицы Софии🤩Наша землячка из г. Вологда🔥 Мать героиня😎💪🏼Словила неожиданное вдохновение 👻 и пришла за новой профессией‼️\n' +
         '\n' +
-        'Инста Софии: sofi_raposa_tattoo👀'
+        '👉🏼Инста Софии: sofi_raposa_tattoo👀'
     },
     {
         file_id: 'BAACAgIAAxkBAAM6Yfgit2mFKsPb4EVdc779VYw6i-EAAsMXAAJ-N8FLDi2UTAoGtjEjBA',
