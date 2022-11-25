14f7e50587e047e0fe11200d5a0d6fa4597f3f47
Nikolai Doinikov <56079901+Nobodypfct@users.noreply.github.com>
2022-11-25 14:58:58 +0300

    minor updates

--- index.js
+++ index.js
@@ -1,109 +1,139 @@
-const TelegramApi = require('node-telegram-bot-api'); 
-const http = require('http');
-const { yearOfReviews, firstScreenKeyboardText, descriptionDocumentToken, welcomeMessage, errorMessage } = require('./constants/constants');
-const { reviews2020 } = require('./constants/reviewsData2020');
-const { reviews2021 } = require('./constants/reviewsData2021');
+const TelegramApi = require("node-telegram-bot-api");
+const http = require("http");
+const {
+  yearOfReviews,
+  firstScreenKeyboardText,
+  descriptionDocumentToken,
+  welcomeMessage,
+  errorMessage,
+} = require("./constants/constants");
+const { reviews2020 } = require("./constants/reviewsData2020");
+const { reviews2021 } = require("./constants/reviewsData2021");
 
-const token = '5121108812:AAFHWX6dUkS-06yvTcJcw0XqWA97f-aNB7c'; // kolnya_school_bot
-// const token = '5123162187:AAHondwdhh8e7YAx_-2c-7YrJPAezk4mwI8'; //test bot
+// const token = '5121108812:AAFHWX6dUkS-06yvTcJcw0XqWA97f-aNB7c'; // kolnya_school_bot
+const token = "5123162187:AAHondwdhh8e7YAx_-2c-7YrJPAezk4mwI8"; //test bot
 
 const port = process.env.PORT || 8000;
 
 const server = http.createServer(async (req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
-  });
-
-server.listen(port, () => {
-    console.log("App is running on port " + port);
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello World");
 });
 
-const bot = new TelegramApi(token, { polling: true })
+server.listen(port, () => {
+  console.log("App is running on port " + port);
+});
+
+const bot = new TelegramApi(token, { polling: true });
 
 bot.setMyCommands([
-    {command: '/start', description: 'Старт'}, 
-    // {command: '/info', description: 'Получить какую-нибудь инфо'}
-])
+  { command: "/start", description: "Старт" },
+  // {command: '/info', description: 'Получить какую-нибудь инфо'}
+]);
 
 const firstScreenKeyboard = [
-    [{text: firstScreenKeyboardText.description, callback_data: '1'}],
-    [{text: firstScreenKeyboardText.reviews, callback_data: '2'}],
-]
+  [{ text: firstScreenKeyboardText.description, callback_data: "1" }],
+  [{ text: firstScreenKeyboardText.reviews, callback_data: "2" }],
+];
 
 const firstScreenBtnOptions = {
-    reply_markup: JSON.stringify({
-        resize_keyboard: true,
-        keyboard: firstScreenKeyboard,
-    }) 
-}
+  reply_markup: JSON.stringify({
+    resize_keyboard: true,
+    keyboard: firstScreenKeyboard,
+  }),
+};
 
 const reviewsKeyboard = [
-    [{text: '2020', callback_data: yearOfReviews.reviewsFrom2020}],
-    [{text: '2021', callback_data: yearOfReviews.reviewsFrom2021}],
-    [{text: 'Назад', callback_data: 'Back'}]
-]
+  [{ text: "2020", callback_data: yearOfReviews.reviewsFrom2020 }],
+  [{ text: "2021", callback_data: yearOfReviews.reviewsFrom2021 }],
+  [{ text: "Назад", callback_data: "Back" }],
+];
 
 const reviewsBtnOptions = {
-    reply_markup: JSON.stringify({
-        resize_keyboard: true,
-        keyboard: reviewsKeyboard,
-    })
-}
+  reply_markup: JSON.stringify({
+    resize_keyboard: true,
+    keyboard: reviewsKeyboard,
+  }),
+};
 
-bot.on('message', async (msg) => {
-    const text = msg.text;
-    const chatId = msg.chat.id;
+bot.on("message", async (msg) => {
+  const text = msg.text;
+  const chatId = msg.chat.id;
 
-    // commands
-    if (text === '/start') {
-        // await bot.sendSticker
-        return bot.sendMessage(chatId, welcomeMessage, firstScreenBtnOptions)
-    }
+  // commands
+  if (text === "/start") {
+    // await bot.sendSticker
+    return bot.sendMessage(chatId, welcomeMessage, firstScreenBtnOptions);
+  }
 
-    // click on first screen btns
-    switch (text) {
-        case firstScreenKeyboardText.description:
-            await bot.sendDocument(chatId, descriptionDocumentToken)
-            return bot.sendMessage(chatId, 'Задать вопросы или записаться на курс 👉🏼 @hvatiit_maks', firstScreenBtnOptions)
-            
-        case firstScreenKeyboardText.reviews:
-            return bot.sendMessage(chatId, 'Пожалуйста, выберите один из вариантов:', reviewsBtnOptions)
-        default:
-            break;
-    }
+  // click on first screen btns
+  switch (text) {
+    case firstScreenKeyboardText.description:
+      await bot.sendDocument(chatId, descriptionDocumentToken);
+      return bot.sendMessage(
+        chatId,
+        "Задать вопросы или записаться на курс 👉🏼 @hvatiit_maks",
+        firstScreenBtnOptions
+      );
 
-    const functionThatReturnsAPromise = async i => { //a function that returns a promise
-        await bot.sendVideo(chatId, i.file_id, { caption: i.caption, reply_markup: reviewsBtnOptions })
-        return Promise.resolve('ok')
-    }
+    case firstScreenKeyboardText.reviews:
+      return bot.sendMessage(
+        chatId,
+        "Пожалуйста, выберите один из вариантов:",
+        reviewsBtnOptions
+      );
+    default:
+      break;
+  }
 
-    const doSomethingAsync = async item => {
-        return functionThatReturnsAPromise(item)
-    }
+  const functionThatReturnsAPromise = async (i) => {
+    //a function that returns a promise
+    await bot.sendVideo(chatId, i.file_id, {
+      caption: i.caption,
+      reply_markup: reviewsBtnOptions,
+    });
+    return Promise.resolve("ok");
+  };
 
-    const sendReviews = async (list) => {
-        return Promise.all(list.map(item => doSomethingAsync(item)))
-    }
+  const doSomethingAsync = async (item) => {
+    return functionThatReturnsAPromise(item);
+  };
 
-    // click on reviews btns
-    switch (text) {
-        case '2020':
-            return sendReviews(reviews2020).then(data => {
-                return bot.sendMessage(chatId, 'Задать вопросы или записаться на курс 👉🏼 @hvatiit_maks', reviewsBtnOptions)
-            })
-        case '2021':
-            return sendReviews(reviews2021).then(data => {
-                return bot.sendMessage(chatId, 'Задать вопросы или записаться на курс 👉🏼 @hvatiit_maks', reviewsBtnOptions)
-            })      
-        case 'Назад': 
-            return bot.sendMessage(chatId, 'Пожалуйста, выберите один из вариантов:', firstScreenBtnOptions)
-        default:
-            break;
-    }
-    
-    console.log('msg', msg, msg.text)
+  const sendReviews = async (list) => {
+    return Promise.all(list.map((item) => doSomethingAsync(item)));
+  };
 
-    // if the user sent another message
-    return bot.sendMessage(chatId, errorMessage)
-})
+  // click on reviews btns
+  switch (text) {
+    case "2020":
+      return sendReviews(reviews2020).then((data) => {
+        return bot.sendMessage(
+          chatId,
+          "Задать вопросы или записаться на курс 👉🏼 @hvatiit_maks",
+          reviewsBtnOptions
+        );
+      });
+    case "2021":
+      return sendReviews(reviews2021).then((data) => {
+        return bot.sendMessage(
+          chatId,
+          "Задать вопросы или записаться на курс 👉🏼 @hvatiit_maks",
+          reviewsBtnOptions
+        );
+      });
+    case "Назад":
+      return bot.sendMessage(
+        chatId,
+        "Пожалуйста, выберите один из вариантов:",
+        firstScreenBtnOptions
+      );
+    default:
+      break;
+  }
+
+  console.log("msg", msg, msg.text);
+
+  // if the user sent another message
+  return bot.sendMessage(chatId, errorMessage);
+});
